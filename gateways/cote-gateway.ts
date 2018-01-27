fee08b9dac64bed2b5e749c100a163e00334e5dc
Brice Carpentier <brice@daknet.org>
2018-01-27 18:13:47 +0100

    added cote gateway

---
+++ gateways/cote-gateway.ts
@@ -0,0 +1,38 @@
+import { Publisher, Responder, Event as CoteEvent } from 'cote';
+
+import { Datastore, NEW_EVENT } from '../core/datastore';
+import { Gateway } from '../core/gateway';
+import { TocqEvent } from '../core/tocq-event';
+
+const FETCH:string = 'tocq:fetch';
+
+interface CoteGatewayEvent extends CoteEvent {
+  from: number,
+  limit?: number,
+}
+
+export default class CoteGateway implements Gateway {
+  datastore: Datastore;
+  publisher: Publisher;
+  responder: Responder;
+
+  constructor(datastore: Datastore) {
+    this.datastore = datastore;
+    this.datastore.addListener(NEW_EVENT, this.onNewEvent);
+    this.publisher = new Publisher({ name: 'tocq:publisher' });
+    this.responder = new Responder({ name: 'tocq:responder' });
+    this.responder.on(FETCH, this.onFetchRequest);
+  }
+
+  onNewEvent(event: TocqEvent) {
+    this.publisher.publish(NEW_EVENT, event);
+  }
+
+  onFetchRequest(event: CoteGatewayEvent): Promise<Array<TocqEvent>> { 
+    return this.datastore.fetch(event.from, event.limit);
+  }
+}
+
+export {
+  FETCH,
+};
