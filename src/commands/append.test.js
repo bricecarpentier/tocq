b74153fdf072b3a72166973ec866b98795111459
Brice Carpentier <brice@daknet.org>
2018-08-13 11:46:41 +0100

    improve append testing

--- src/commands/append.test.js
+++ src/commands/append.test.js
@@ -1,39 +1,44 @@
 const append = require('./append');
 
 
-describe('append', async () => {
-  const logs = {
-    append: jest.fn().mockResolvedValue(42),
-  }
+const getLogs = () => ({
+  append: jest.fn().mockResolvedValue(42),
+});
+
+
+const getReply = () => ({ code: jest.fn() });
+
+
+test('when called with an FSA', async () => {
+  expect.assertions(3);
+  const body = {
+    type: 'ACTION_TYPE',
+    payload: {
+      message: 'so far so good',
+    },
+  };
+  const request = { body };
+  const reply = getReply();
+  const logs = getLogs();
   const uut = append({ logs });
+  const response = await uut(request, reply);
+  expect(logs.append).toHaveBeenLastCalledWith('default', body);
+  expect(response).toEqual({ ...body, sequenceId: 42 });
+  expect(reply.code).not.toHaveBeenCalled();
+});
 
 
-  test('when called with an FSA', async () => {
-    expect.assertions(2);
-    const body = {
-      type: 'ACTION_TYPE',
-      payload: {
-        message: 'so far so good',
-      },
-    };
-    const request = { body };
-    const reply = { code: jest.fn() };
-
-    const response = await uut(request, reply);
-    expect(response).toEqual({ ...body, sequenceId: 42 });
-    expect(reply.code).not.toHaveBeenCalled();
-  });
-
-
-  test('when called with something else than an FSA', async () => {
-    expect.assertions(2);
-    const body = {
-      foo: 'bar',
-    };
-    const request = { body };
-    const reply = { code: jest.fn() };
-    const response = await uut(request, reply);
+test('when called with something else than an FSA', async () => {
+  expect.assertions(3);
+  const body = {
+    foo: 'bar',
+  };
+  const request = { body };
+  const reply = getReply();
+  const logs = getLogs();
+  const uut = append({ logs });
+  const response = await uut(request, reply);
+  expect(logs.append).not.toHaveBeenCalled();
   expect(response).toEqual({ message: 'request.body should be a FSA' });
   expect(reply.code).toHaveBeenCalledWith(400);
-  });
 });
